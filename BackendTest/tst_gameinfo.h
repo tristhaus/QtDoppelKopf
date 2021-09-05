41c550aaf04a1cdaa6b7aec009a5342e0cd02f15
tristhaus <tristhaus@yahoo.de>
2021-09-05 20:49:01 +0200

    Fix incorrect name in copyright notice

--- BackendTest/tst_gameinfo.h
+++ BackendTest/tst_gameinfo.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of QtPollyNom.
+ * This file is part of QtDoppelKopf.
  *
- * QtPollyNom is free software: you can redistribute it and/or modify
+ * QtDoppelKopf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * QtPollyNom is distributed in the hope that it will be useful,
+ * QtDoppelKopf is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with QtPollyNom.  If not, see <http://www.gnu.org/licenses/>.
+ * along with QtDoppelKopf.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
